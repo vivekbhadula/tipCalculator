f9016654a58bb3b0943cc8f92de41b0cac5d0dcf
vivekbhadula <vivekbhadula201420@gmail.com>
2021-03-06 22:15:31 +0530

    connected iboutlets.

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var zeroPctButton: UIButton!
+    
+    @IBOutlet weak var tenPctButton: UIButton!
+    
+    @IBOutlet weak var twentyPctButton: UIButton!
+    
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
