af292a1fe81ba010812e5ff6027f3c547bdef8d1
vivekbhadula <vivekbhadula201420@gmail.com>
2021-03-07 12:41:03 +0530

    updated

---
+++ Tipsy/Controller/ResultsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Vivek Bhadula on 07/03/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
